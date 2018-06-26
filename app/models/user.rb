2488bc2d3fa0b5ebe29c7ae4ffe01bb2da061efd
GokiSakuraba <goki14914@icloud.com>
2018-06-27 01:06:25 +0900

    add can_destroy_foreign_key

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :messages
-  has_many :room_users
+  has_many :room_users, dependent: :destroy
   has_many :rooms, through: :room_users
 
     ##
