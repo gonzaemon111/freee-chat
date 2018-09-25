6072361b08b8ef61d7135b5c5e9fe7cc8e121d04
GokiSakuraba <goki14914@icloud.com>
2018-09-25 22:22:43 +0900

    fix some migration file

--- db/migrate/20180625063838_remove_user_to_messages.rb
+++ db/migrate/20180625063838_remove_user_to_messages.rb
@@ -1,6 +1,6 @@
 class RemoveUserToMessages < ActiveRecord::Migration[5.1]
   def change
-    remove_reference :messages, :room
+    remove_reference :messages, :room, foreign_key: true
     add_column :messages, :room_id, :integer
   end
 end
